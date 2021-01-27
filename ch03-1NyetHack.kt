b9e964042b4ec21db7edd0cf36f6c774fe0fa877
unknown <jasongu@mail.lhu.edu.tw>
2021-01-27 22:27:31 +0800

    ch03 to ch03-1

---
+++ ch03-1NyetHack.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val name = "Madrigal"
+    var healthPoints = 100
+
+    if (healthPoints == 100){
+        println(name+"健康狀態極佳")
+    } else {
+        println(name+"健康狀態不妙")
+    }
+}
